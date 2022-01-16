cb33e3173331def220ee36bd22fbdcf677ca299e
ranjithakumari <54302203+ranjithakumari@users.noreply.github.com>
2022-01-16 21:26:19 +0530

    Add files via upload

---
+++ account.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+var app     = express();
+//const postControllers = require("../controllers/postControllers");
+var dal     = require('./dal.js');
+
+module.exports =app;
